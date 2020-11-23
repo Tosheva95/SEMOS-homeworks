b3d80566b29a9b9da8691e22df2bf40b896d29c8
Tosheva95 <72809984+Tosheva95@users.noreply.github.com>
2020-11-23 15:08:27 +0100

    Add files via upload

---
+++ js-homework-2/JS-homework-2/palindromes.js
@@ -0,0 +1,16 @@
+
+var list = ['Македонија','анаволимилована','дај ми една чаша полна со вино','радар','кајак', 'калабалак','нели не си ти сенилен','јадење и пиење дај','слушател','фудбал','музика'];
+
+function findPalindrome(list) {
+		let palindromes = [];
+		
+    for (let i = 0; i < list.length; i++) {
+				let pal = list[i].split('').reverse().join('');
+        if (pal.replaceAll(' ', '') === list[i].replaceAll(' ', '')) {
+            palindromes.push(list[i]);
+        }
+    }
+    console.log(palindromes);
+}
+
+findPalindrome(list);
