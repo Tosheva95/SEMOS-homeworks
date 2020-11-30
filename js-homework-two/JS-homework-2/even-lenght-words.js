fa96912e0bc7e8d6341a8006010dc2a143ff80ae
Tosheva95 <72809984+Tosheva95@users.noreply.github.com>
2020-11-30 18:12:41 +0100

    Update even-lenght-words.js
    По ваше барање, два for loop споени во една функција.

--- js-homework-two/JS-homework-2/even-lenght-words.js
+++ js-homework-two/JS-homework-2/even-lenght-words.js
@@ -1,30 +1,31 @@
 var str = 'I am a student in Computer Science and JavaScript academy at Semos Education. I am taking a course for JavaScript currently. My goals are to become a Software Developer after that, so I am working toward it right now.'
 
 var str1 = str.split(' ');
-console.log(str1); 
-
 
 function myFunction(array) {
-	let arr = []
+  let arr = []
+  let evenStrings = str1;
+  let occurencies = new Object();
+  
+  
 	for (let i = 0; i < array.length; i++) {
 		if (array[i].length % 2 === 0) {
 		arr.push(array[i]);
 		}
-	}
-	return arr
+  };
+
+  for (let i = 0; i < evenStrings.length; i++) {
+    if(occurencies[evenStrings[i]] != null) {
+      occurencies[evenStrings[i]] += 1;
+    } else {
+      occurencies[evenStrings[i]] = 1;
+    }
+  }
+	return {
+    arr,
+    occurencies
+  }
 }
 console.log(myFunction(str1));
 
 
-let evenStrings = myFunction(str1);
-let occurencies = new Object();
-
-
-for (let i = 0; i < evenStrings.length; i++) {
-	if(occurencies[evenStrings[i]] != null) {
-		occurencies[evenStrings[i]] += 1;
-	} else {
-		occurencies[evenStrings[i]] = 1;
-	}
-}
-console.log(occurencies); 
