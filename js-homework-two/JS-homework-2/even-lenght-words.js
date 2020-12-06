67fa4eeb6cf915a671befd176bb9369a91671025
Tosheva95 <72809984+Tosheva95@users.noreply.github.com>
2020-12-06 22:26:34 +0100

    Update even-lenght-words.js

--- js-homework-two/JS-homework-2/even-lenght-words.js
+++ js-homework-two/JS-homework-2/even-lenght-words.js
@@ -3,25 +3,25 @@ var str = 'I am a student in Computer Science and JavaScript academy at Semos Ed
 var str1 = str.split(' ');
 
 function myFunction(array) {
-  let arr = []
+  let arr = [];
   let evenStrings = str1;
   let occurencies = new Object();
   
   
-	for (let i = 0; i < array.length; i++) {
+for (let i = 0; i < array.length; i++) {
 		if (array[i].length % 2 === 0) {
 		arr.push(array[i]);
 		}
   };
 
-  for (let i = 0; i < evenStrings.length; i++) {
+for (let i = 0; i < evenStrings.length; i++) {
     if(occurencies[evenStrings[i]] != null) {
       occurencies[evenStrings[i]] += 1;
     } else {
       occurencies[evenStrings[i]] = 1;
     }
   }
-	return {
+return {
     arr,
     occurencies
   }
