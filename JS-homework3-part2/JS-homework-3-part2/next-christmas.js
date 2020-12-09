8458fd110e2662a1a60c4e1796d1e5cd8508a27b
Tosheva95 <72809984+Tosheva95@users.noreply.github.com>
2020-12-09 22:14:08 +0100

    Update next-christmas.js

--- JS-homework3-part2/JS-homework-3-part2/next-christmas.js
+++ JS-homework3-part2/JS-homework-3-part2/next-christmas.js
@@ -1,15 +1,12 @@
-function christmas() { 
-
-today=new Date();
-  var cmas=new Date(today.getFullYear(), 11, 31);
-  if (today.getMonth()==11 && today.getDate()>31) 
-  {
-    cmas.setFullYear(cmas.getFullYear()+1); 
-  }  
-  var one_day=1000*60*60*24;
-console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+" days left until Christmas!"); 
-
-}
-
-christmas();
+const christmas = () => {
+  let date1 = new Date('01/07/2022');
+  let date2 = new Date();
+  
+  let difference = date1.getTime() - date2.getTime();
+  
+  let days = Math.ceil(difference / (1000 * 3600 * 24));
+  return days;
+  }
+  
+  console.log(christmas() + ' days until Christmas');
 
